2a5ce88463732a12db7eea820995dc26f70014e2
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-09-04 22:55:47 -0700

    app.php create routes and infor needed at top to make it all function correctly

--- app/app.php
+++ app/app.php
@@ -1,41 +1,39 @@
 <?php
+      // needed in intital set up
+      require_once __DIR__."/../vendor/autoload.php";
+      // class
+      require_once __DIR__."/../src/Contact.php";
 
-    //App dependencies
-    require_once __DIR__."/../vendor/autoload.php";
-    // dependency needed for each Class, etc.
-    require_once __DIR__."/../src/Class1.php";
-    // require_once __DIR__."/../src/Class2.php";
+      // saves an empty array list_of_contacts
+      session_start();
+      if(empty($_SESSION['list_of_contacts']))
+      {
+        $_SESSION['list_of_contacts'] = array();
+      }
+      // loades a new silex app
+      $app = new Silex\Application();
 
-    // Tells the app to use Silex Application
-    $app = new Silex\Application();
+      // needed for twig to use it
+      $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+      ));
+    //renders the main page displaying all contacts in the address book
+      $app->get("/", function() use ($app) {
+        return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
+      });
 
-    //Tells app how to access sql phpmyadmin database
-    // $server = 'mysql:host=localhost;dbname=hair_salon';
-    // $username = 'root';
-    // $password = 'root';
-    // $DB = new PDO($server, $username, $password);
+    //renders the page to create a new contact, when new info is entered it is posted and displays
+      $app->post("/contacts", function() use ($app) {
+        $contact = new Contact($_POST['name'], $_POST['phone_number'], $_POST['address']);
+        $contact->save();
+        return $app['twig']->render('create_contact.html.twig', array('new_contact' => $contact));
+      });
 
-    //Points app to twig templates
-    // $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    //     'twig.path' => __DIR__.'/../views'
-    // ));
-
-    //Still not sure what this does, think it has something to with Symfony being part of the composer package download list. Seems like it Symfony is used to override method paramaters in HTTP?
-    use Symfony\Component\HttpFoundation\Request;
-    Request::enableHttpMethodParameterOverride();
-
-    //Path to homepage
-    //C[R]UD, Read, get
-    // tells app to render index.html.twig with twig and to getALL from the variable listed in the array. that variable needs to be within the Class
-    // $app->get("/", function() use ($app) {
-    //     return $app['twig']->render('index.html.twig', array('variable class name?' => Class::getAll()));
-    // });
-
- $app->get("/", function(){
-return "helloooooooo world";
-
-
-});
-
-return $app;
+    //renders the page to delete contacts, this then clear the list of contacts
+      $app->post("/delete_contacts", function () use ($app) {
+        Contact::deleteAll();
+        return $app['twig']->render('delete_contacts.html.twig');
+      });
+      // needed at bottom 
+      return $app;
 ?>
